c956eb69133fd533ce9db3cf13b0d82467d65872
unith16 <unithkumar@gmail.com>
2020-08-23 22:43:40 -0400

    Go- Updated code for test scripts

--- life_test.go
+++ life_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func goTest(t *testing.T){
 	var result int
-	result = test(3)
-	if(result != 4){
-		t.Errorf("Actual - %d ; Expected - 4", result );
+	result = test()
+	if(result != 1){
+		t.Errorf("Actual - %d ; Expected - 1", result );
 	}
 }
