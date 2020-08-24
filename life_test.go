7789e7e7419a19e89a77a5f6584e3b20b80c1730
unithmallavaram <51611862+unithmallavaram@users.noreply.github.com>
2020-08-23 22:39:14 -0400

    Update life_test.go

--- life_test.go
+++ life_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func goTest(t *testing.T){
-	result := test(3)
+	var result int
+	result = test(3)
 	if(result != 4){
 		t.Errorf("Actual - %d ; Expected - 4", result );
 	}
