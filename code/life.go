f4538740e7baf5b311677066cda526f97f5521f9
unithmallavaram <51611862+unithmallavaram@users.noreply.github.com>
2020-08-24 20:24:07 -0400

    Go - Added a bug

--- code/life.go
+++ code/life.go
@@ -57,7 +57,7 @@ func live(a []int, r, gen int)int{
 	neighbors = 0
 	var b []int
 	for c:=0; c<len(a); c++ {
-		if(c-1 >= 0 && c%r != 0 && c-1<=len(a)-1){
+		if(c-1 >= 0 && c-1<=len(a)-1){
 			neighbors += a[c-1]
 		}
 		if(c+1 >= 0 && (c+1)%r != 0 && c+1<=len(a)-1){
